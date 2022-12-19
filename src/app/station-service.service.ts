0063b12d84e6cb7a52a769d493faa65c3e897ee2
OmarYasser21 <94626701+OmarYasser21@users.noreply.github.com>
2022-12-19 16:06:41 +0200

    Update station-service.service.ts

--- src/app/station-service.service.ts
+++ src/app/station-service.service.ts
@@ -8,7 +8,7 @@ import { Station } from './station';
 })
 export class StationService {
 
-  private stationUrl = 'http://localhost:8080/stations';
+  private stationUrl = 'https://backend-p3.apps.eu410.prod.nextcle.com/stations';
 
   constructor(private http: HttpClient) { }
 
